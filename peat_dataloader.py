48eff50d6aa3fa607bd91b0849ff5aa816cbb39d
ajravikumar <ajayravikumar007@gmail.com>
2022-09-07 11:35:03 +0100

    Jaccard index

--- peat_dataloader.py
+++ peat_dataloader.py
@@ -17,16 +17,30 @@ from torchvision import transforms
 
 
 class PeatDataset(torch.utils.data.Dataset):
-    def __init__(self):
+    def __init__(self, data_split):
 
-        self.images = os.listdir("/home/ajay/Documents/dataset_tif/final_data")
-        self.masks = os.listdir("/home/ajay/Documents/dataset_mask/final_data")
+        if data_split ==  'train' :
+
+            self.images = os.listdir("/home/ajay/Documents/dataset_tif/split_dataset/train/dataset")
+            self.masks = os.listdir("/home/ajay/Documents/dataset_mask/split_dataset/train/dataset")
+
+        if data_split ==  'val' :
+
+            self.images = os.listdir("/home/ajay/Documents/dataset_tif/split_dataset/val/dataset")
+            self.masks = os.listdir("/home/ajay/Documents/dataset_mask/split_dataset/val/dataset")
+
+        if data_split ==  'test' :
+
+            self.images = os.listdir("/home/ajay/Documents/dataset_tif/split_dataset/test/dataset")
+            self.masks = os.listdir("/home/ajay/Documents/dataset_mask/split_dataset/test/dataset")
 
         assert len(self.images)==len(self.masks), "lengths are not matching"
         
 
-
+        # mean = (0.5, 0.5, 0.5)
+        # std = (0.5, 0.5, 0.5)
         self.transform=transforms.Compose([
+            # transforms.normalize(mean, std),
             transforms.Resize(256),
             transforms.ToTensor()
         ])
@@ -39,10 +53,25 @@ class PeatDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
 
         
-        image_path = os.path.join("/home/ajay/Documents/dataset_tif/final_data", self.images[idx])
-        mask_path = os.path.join("/home/ajay/Documents/dataset_mask/final_data", self.images[idx])
+        image_path = os.path.join("/home/ajay/Documents/dataset_tif/dataset_256/dataset", self.images[idx])
+        mask_path = os.path.join("/home/ajay/Documents/dataset_mask/dataset_256/dataset", self.images[idx])
 
-       
+        # import pdb; pdb.set_trace()
+
+        # if self.data_split ==  'train' :
+
+        #     image_path = os.path.join("/home/ajay/Documents/dataset_tif/split_dataset/train/dataset", self.images[idx])
+        #     mask_path = os.listdir("/home/ajay/Documents/dataset_mask/split_dataset/train/dataset", self.images[idx])
+
+        # if self.data_split ==  'val' :
+
+        #     image_path = os.path.join("/home/ajay/Documents/dataset_tif/split_dataset/val/dataset", self.images[idx])
+        #     mask_path = os.listdir("/home/ajay/Documents/dataset_mask/split_dataset/val/dataset", self.images[idx])
+
+        # if self.data_split ==  'test' :
+
+        #     image_path = os.path.join("/home/ajay/Documents/dataset_tif/split_dataset/test/dataset", self.images[idx])
+        #     mask_path = os.listdir("/home/ajay/Documents/dataset_mask/split_dataset/test/dataset", self.images[idx])
         
 
         image = Image.open(image_path)
--- peat_trainer.py
+++ peat_trainer.py
@@ -1,10 +1,13 @@
 import os
+# from random import sample
 import torch
 import time
 import matplotlib.pyplot as plt
 import pytorch_lightning as pl
 import segmentation_models_pytorch as smp
 import numpy as np
+
+
 
 from sklearn.model_selection import train_test_split
 
@@ -18,7 +21,7 @@ from torchvision.utils import save_image
 import neptune.new as neptune
 
 
-run = neptune.init(project='ajravikumar/peat-segmentation')
+# run = neptune.init(project='ajravikumar/peat-segmentation')
 
 params = {
     "batch_size": 16,
@@ -27,22 +30,37 @@ params = {
     "criterion": "DiceLoss",
     "resolution": "256x256"
 }
-run["parameters"] = params
+# run["parameters"] = params
 
 
 
-train_dataset = PeatDataset()
+train_dataset = PeatDataset('train')
+val_dataset = PeatDataset('val')
+test_dataset = PeatDataset('test')
 
 # import pdb; pdb.set_trace()
 
-train_data, val_data = train_test_split(train_dataset, test_size=0.25)
+# Assert datasets dont intersect with each other
+assert set(test_dataset.images).isdisjoint(set(train_dataset.images))
+assert set(test_dataset.images).isdisjoint(set(val_dataset.images))
+assert set(train_dataset.images).isdisjoint(set(val_dataset.images))
 
-print(len(train_dataset))
-print(len(train_data))
-print(len(val_data))
+print(f'Number of training samples: {len(train_dataset)}')
+print(f'Number of validation samples: {len(val_dataset)}')
+print(f'Number of test samples: {len(test_dataset)}')
 
-train_dataloader = DataLoader(train_data, batch_size=16, shuffle=True, num_workers=8, drop_last=True)
-val_dataloader = DataLoader(val_data, batch_size=16, num_workers=8, drop_last=True)
+train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=8, drop_last=True)
+val_dataloader = DataLoader(val_dataset, batch_size=64, num_workers=8, drop_last=True)
+test_dataloader = DataLoader(test_dataset, batch_size=32, num_workers=8, drop_last=True)
+
+# sample = train_dataset.images
+# # plt.subplot(1,2,1)
+# # plt.imshow(sample["image"].transpose(1, 2, 0)) # for visualization we have to transpose back to HWC
+# # plt.subplot(1,2,2)
+# # plt.imshow(sample["mask"].squeeze())  # for visualization we have to remove 3rd dimension of mask
+# # plt.show()
+
+# import pdb; pdb.set_trace()
 
 device = torch.device("cuda:0") 
 
@@ -54,8 +72,10 @@ criterion = smp.losses.DiceLoss(mode='binary', log_loss=True)
 
 criterion=criterion.to(device)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+learning_rate = 1e-3
 
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5, threshold= 1e-3 )
 best_loss = float('inf')
 
 model_version = '0.00'
@@ -64,12 +84,12 @@ model_version = '0.00'
 
 def train_model(model, train_dataloader, optimizer, epoch):
     _ = model.train()
-    loss_total = 0
-    total = 0 
+     
     if torch.cuda.is_available():
         model.cuda()
     
     losses = []
+    iou = []
 
     for image,mask in train_dataloader:
         optimizer.zero_grad()
@@ -81,34 +101,48 @@ def train_model(model, train_dataloader, optimizer, epoch):
 
         prediction = model.forward(image)
 
-        # print(prediction.shape)
-        # import pdb; pdb.set_trace() 
-        
         
         loss = criterion(prediction, mask)
 
-        # loss_total += loss/16
         
-        print(loss.item())
+        out = (prediction > 0.5).int()
+        mask1 = (mask > 0.5).int()
+        # import pdb; pdb.set_trace()
+        tp, fp, fn, tn = smp.metrics.functional.get_stats(out, mask1, mode='binary', threshold = None)
+        iou_score = smp.metrics.functional.iou_score(tp, fp, fn, tn, reduction='micro')
+
+        # print(f'IOU score is {iou_score}')
+
+        # print(loss.item())
 
         loss_value = loss.item()
         losses.append(loss_value)
 
+        iou_value = iou_score.item()
+        iou.append(iou_value)
         
 
         loss.backward()
         optimizer.step()
         
-    save_image(prediction, f'outputs/prediction{epoch}.png')
-    save_image(image, f'outputs/image{epoch}.png')
-    save_image(mask, f'outputs/mask{epoch}.png')
+    # save_image(prediction, f'outputs/prediction{epoch}.png')
+    # save_image(image, f'outputs/image{epoch}.png')
+    # save_image(mask, f'outputs/mask{epoch}.png')
 
     train_loss_epoch = np.round(np.mean(losses), 4)
+    train_iou = np.round(np.mean(iou) ,4)
+    # print('''[Epoch: {0} / {1} | avg train loss {2} | lr : {3}'''.
+    #               format(
+    #                   epoch + 1,
+    #                   30,
+    #                   np.round(np.mean(losses), 4),
+    #                   1e-3
+    #               ))
     
-    return train_loss_epoch
+    return train_loss_epoch, train_iou
 
 
-def val_model (model, val_dataloader, optimizer, epoch):
+def val_model (model, val_dataloader, epoch):
 
     _ = model.eval()
 
@@ -116,47 +150,80 @@ def val_model (model, val_dataloader, optimizer, epoch):
         model.cuda()
     
     losses = []
+    iou = []
 
-    for image,mask in val_dataloader:
-        
-        if torch.cuda.is_available():
-            image = image.cuda()
-            mask = mask.cuda()
+    with torch.no_grad():
+        for image,mask in val_dataloader:
             
+            if torch.cuda.is_available():
+                image = image.cuda()
+                mask = mask.cuda()
+                
 
-        prediction = model.forward(image)
+            prediction = model.forward(image)
+            
+            
+            loss = criterion(prediction, mask)
+
+            # loss_total += loss/16
+            
+            # print(loss.item())
+
+            out = (prediction > 0.5).int()
+            mask1 = (mask > 0.5).int()
+            
+            tp, fp, fn, tn = smp.metrics.functional.get_stats(out, mask1, mode='binary', threshold = None)
+            iou_score = smp.metrics.functional.iou_score(tp, fp, fn, tn, reduction='micro')
+
+            loss_value = loss.item()
+            losses.append(loss_value)
+
+            iou_value = iou_score.item()
+            iou.append(iou_value)
+
         
-        
-        loss = criterion(prediction, mask)
 
-        # loss_total += loss/16
-        
-        # print(loss.item())
-
-        loss_value = loss.item()
-        losses.append(loss_value)
-
-        
-
-        loss.backward()
-        optimizer.step()
+        # loss.backward()
+        # optimizer.step()
     
+    save_image(prediction, f'outputs/prediction{epoch}.png')
+    save_image(image, f'outputs/image{epoch}.png')
+    save_image(mask, f'outputs/mask{epoch}.png')
+
+
     val_loss_epoch = np.round(np.mean(losses), 4)
+    val_iou = np.round(np.mean(iou) ,4)
     
-    return val_loss_epoch
+    return val_loss_epoch, val_iou
 
 
+num_epoch = 50
 
-for epoch in range(50):
-    loss = train_model(model, train_dataloader, optimizer, epoch )
+for epoch in range(num_epoch):
 
-    val_loss = val_model(model, val_dataloader, optimizer, epoch)
+    # print (f'MODEL TRAINING EPOCH:{epoch}')
+    loss, iou = train_model(model, train_dataloader, optimizer, epoch )
 
-    run["train/loss"].log(loss)
-    run["validation/loss"].log(val_loss)
+    val_loss, val_iou = val_model(model, val_dataloader, epoch)
+
+    # run["train/loss"].log(loss)
+    # run["validation/loss"].log(val_loss)
     # run.stop()
 
-    print('total loss is' , loss.item())
+    # print('total loss is' , loss.item())
+
+    print('''[Epoch: {0} / {1} | avg train loss {2}  | avg train iou {3} | avg validation loss {4} | avg validation iou {5}| lr : {6}'''.
+                  format(
+                      epoch + 1,
+                      num_epoch,
+                      loss,
+                      iou,
+                      val_loss,
+                      val_iou,
+                      optimizer.param_groups[0]['lr']
+                  ))
+
+    scheduler.step(val_loss)              
 
     # if loss.item() < best_loss and loss.item() != 0.0:
     #     best_loss = loss.item()
